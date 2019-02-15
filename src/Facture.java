d3c7f5eed67bb4ae58f8407ab4d4be07a6d69b15
Bruce Ethier <blazier@live.ca>
2019-02-15 17:57:16 -0500

    Probleme avec autre commit, cest lui ou je fini la classe facture et clarifie le code

--- src/Facture.java
+++ src/Facture.java
@@ -5,13 +5,14 @@ public class Facture {
 	public static final String CLIENT = "Clients :", PLAT = "Plats :", COMMANDE = "Commandes :", FIN = "Fin";
 	private ArrayList<String> clients = new ArrayList<>();
 	private ArrayList<Plats> plats = new ArrayList<>();
-	private double[] commandes;
+	private String[][] commandes;
 	
 	public Facture( String[] facture ) {
 		int i = 0;
+		int j = 0;
 
 		if ( CLIENT.compareTo( facture[0] ) == 0 ) {
-			//try {
+	
 			i++;	
 			
 				while ( PLAT.compareTo( facture[i] ) != 0 ) {
@@ -19,15 +20,14 @@ public class Facture {
 
 					i++;
 				}
-				
-				commandes = new double[clients.size()];
+				commandes = new String[clients.size()][2];
 				
 				i++;
 
 				while ( COMMANDE.compareTo( facture[i] ) != 0 ) {
 					String[] temp = facture[i].split( "\u0020" );
 					
-//					plats.add( new Plats(temp[0], Double.parseDouble(temp[1])) );TODO probleme avec les parse???
+					plats.add( new Plats(temp[0], Double.parseDouble(temp[1])) );
 
 					i++;
 				}
@@ -35,22 +35,67 @@ public class Facture {
 				i++;
 
 				while ( FIN.compareTo( facture[i] ) != 0 ) {
+			
 					double prix = 0;
-//					int ind = clients.indexOf( (String) facture[i].split( "\u0020" )[0] );
-//					int nbPlat = Integer.parseInt(facture[i].split( "\u0020" )[2]);
-//					
-//					prix = plats.get( plats.indexOf( new Plats("Poutine", 0) ) ).getCout() * nbPlat;
 					
-					System.out.println( prix );
+					int nbPlat = Integer.parseInt(facture[i].split( "\u0020" )[2]);
+					
+					double prixSimple = 0;
+					
+					for (Plats plats : plats) {
+					
+						if (plats.getNom().compareTo(facture[i].split(" ")[1]) == 0){
+							prixSimple = plats.getCout();
+							
+							break;
+						}
+					}
+					prix = nbPlat * prixSimple;
+					if (j == 0){
+						commandes[j][0] = facture[i].split( "\u0020" )[0];
+						commandes[j][1] = prix+"";
+						j++;
+					} else {
+						boolean dejaLa = false;
+						
+						for (int k = 0; k < commandes.length; k++){
+							
+							try{
+								if (facture[i].split( " " )[0].compareTo(commandes[k][0]) == 0){
+									
+									double temp = Double.parseDouble(commandes[k][1]) + prix;
+									
+									commandes[k][1] = temp+"";
+									
+									dejaLa = true;
+									break;
+								} 
+							}
+							catch (Exception e){
+							}
+						}
+						if (!dejaLa){
+							commandes[j][0] = facture[i].split( "\u0020" )[0];
+							commandes[j][1] = prix+"";
+						}
+						j++;
+					}
 					i++;
 				}
-				
-			//} catch ( IndexOutOfBoundsException e ) {
-				//System.out.println( "Le fichier facture n'est pas formatté correctement." );
-			//}
-
+				afficherCommande();
 		} else {
 			System.out.println( "Le fichier facture n'est pas valide." );
 		}
 	}
+	private void afficherCommande(){
+		
+		System.out.println();
+		
+		for (int i = 0; i < commandes.length; i++) {
+			if (!(commandes[i][0] == null ||  commandes[i][1] == null )){
+				System.out.println( commandes[i][0] + " " + commandes[i][1]);
+			}
+		}
+	}
+	
 }
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,23 @@
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.*;
+
 import outilsjava.OutilsFichier;
 
 public class Main {
 
 	public static void main( String[] args ) {
 
-		System.out.println( "Bienvenue chez Barette!" );
-		
-		String[] compet = {"Melee", "tendinite", "La Mort."};
-		
-		Cv cvBarette = new Cv("Barette", "Jérémy", "Secondaire", "$ARGENT$", -1, compet );
-		
-		cvBarette.affiche();
+
 		
 		
-		
-		String nomFic = OutilsFichier.lireNomFichier("Quelle est le nom du fichier");
+		String nomFic = OutilsFichier.lireNomFichier("Quelle est le nom du fichier: ");
 		try{
 			
 			new Facture((Files.readAllLines( new File(nomFic).toPath(), Charset.defaultCharset() )).toArray(new String[]{}));
 		}
-		catch (Exception e){
+		catch (IOException e){
 		}
 		
 		System.out.println( "\nFin du programme." );
