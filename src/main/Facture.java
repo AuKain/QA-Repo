3650298a0438e86509bcc0816e30bb5f8c68914c
David Ringuet <davidringuet500@gmail.com>
2019-03-25 20:08:13 -0400

    implémentation  des méthodes testerFormat, batirCommande et setErreurs

--- src/main/Facture.java
+++ src/main/Facture.java
@@ -6,14 +6,15 @@ public class Facture {
 
 	public static final String CLIENT = "Clients :", PLAT = "Plats :", COMMANDE = "Commandes :", FIN = "Fin";
 	private ArrayList<String> clients = new ArrayList<>();
-	private ArrayList<Plats> plats = new ArrayList<>(); 
+	private ArrayList<Plats> plats = new ArrayList<>();
 	private String[][] commandes;
+	static String erreurFactures = new String( "" );
 	public static final double TAXE_TPS = 0.05, TAXE_TVQ = 0.10;
 
 	public Facture( String[] facture ) {
 		int i = 0;
 		int j = 0;
-
+		testerFormat( facture, erreurFactures );
 		if ( contientClient( facture[0] ) ) {
 
 			i++;
@@ -30,65 +31,75 @@ public class Facture {
 			while ( !contientCommande( facture[i] ) ) {
 				String[] temp = facture[i].split( "\u0020" );
 
-				plats.add( new Plats( temp[0], Double.parseDouble( temp[1] ) ) );
-
+				try {
+					plats.add( new Plats( temp[0], Double.parseDouble( temp[1] ) ) );
+				} catch ( Exception e ) {
+				}
 				i++;
 			}
 
 			i++;
 
-			while ( !contientFin( facture[i] ) ) {
+			while ( !contientFin( facture[i] ) && facture.length < i ) {
 
-				double prix = 0;
+				if ( !facture[i].isEmpty() ) {
+					double prix = 0;
+					int nbPlat = 0;
+					try {
+						nbPlat = Integer.parseInt( facture[i].split( "\u0020" )[2] );
+					} catch ( Exception e ) {
 
-				int nbPlat = Integer.parseInt( facture[i].split( "\u0020" )[2] );
-
-				double prixSimple = 0; 
-
-				for ( Plats plats : plats ) {
-
-					if ( plats.getNom().compareTo( facture[i].split( " " )[1] ) == 0 ) {
-						prixSimple = plats.getCout();
-
-						break;
 					}
-				}
-				prix = nbPlat * prixSimple;
-				if ( j == 0 ) {
-					commandes[j][0] = facture[i].split( "\u0020" )[0];
-					commandes[j][1] = prix + "";
-					j++;
-				} else {
-					boolean dejaLa = false;
+					double prixSimple = 0;
+					for ( Plats plats : plats ) {
 
-					for ( int k = 0; k < commandes.length; k++ ) {
+						if ( plats.getNom().compareTo( facture[i].split( " " )[1] ) == 0 ) {
+							prixSimple = plats.getCout();
 
-						try {
-							if ( facture[i].split( " " )[0].compareTo( commandes[k][0] ) == 0 ) {
-
-								double temp = Double.parseDouble( commandes[k][1] ) + prix;
-
-								commandes[k][1] = temp + "";
-
-								dejaLa = true;
-								break;
-							}
-						} catch ( Exception e ) {
+							break;
 						}
 					}
-					if ( !dejaLa ) {
+					prix = nbPlat * prixSimple;
+					if ( j == 0 ) {
 						commandes[j][0] = facture[i].split( "\u0020" )[0];
 						commandes[j][1] = prix + "";
+						j++;
+					} else {
+						boolean dejaLa = false;
+
+						for ( int k = 0; k < commandes.length; k++ ) {
+
+							try {
+								if ( facture[i].split( " " )[0].compareTo( commandes[k][0] ) == 0 ) {
+
+									double temp = Double.parseDouble( commandes[k][1] ) + prix;
+
+									commandes[k][1] = temp + "";
+
+									dejaLa = true;
+									break;
+								}
+							} catch ( Exception e ) {
+							}
+						}
+						if ( !dejaLa ) {
+							commandes[j][0] = facture[i].split( "\u0020" )[0];
+							commandes[j][1] = prix + "";
+						}
+						j++;
 					}
-					j++;
 				}
 				i++;
 			}
-		
-			afficherCommande(); 
+			erreurFactures += batirCommande();
+			System.out.println( erreurFactures );
 		} else {
 			System.out.println( "Le fichier facture n'est pas valide." );
 		}
+	}
+
+	private static void setErreurs( String erreurs ) {
+		Facture.erreurFactures += erreurs;
 	}
 
 	public static boolean contientClient( String client ) {
@@ -117,11 +128,11 @@ public class Facture {
 
 	public static boolean voirSiZero( double prix ) {
 		return prix == 0;
-	} 
+	}
 
-	private void afficherCommande() {
-
-		System.out.println();
+	private String batirCommande() {
+		String factureFinale = new String();
+		String commande = new String( "" );
 		boolean present;
 
 		for ( int k = 0; k < clients.size(); k++ ) {
@@ -142,87 +153,112 @@ public class Facture {
 
 			}
 			if ( !present ) {
-				System.out.println( clients.get( k ) + " 0$" );
+				commande += ( clients.get( k ) + " 0$\n" );
 			}
 		}
 
 		for ( int i = 0; i < commandes.length; i++ ) {
 			if ( !( commandes[i][0] == null || commandes[i][1] == null ) ) {
-				System.out.println( commandes[i][0] + " " + commandes[i][1] + "$" );
+				commande += ( commandes[i][0] + " " + commandes[i][1] + "$\n" );
 			}
 		}
+		return ( factureFinale += commande );
 	}
 
-	public static int testerFormat(String[] facture){
+	public static int testerFormat( String[] facture, String erreursFacture ) {
 		int retour = -1;
 		ArrayList<String> clientsTemp = new ArrayList<String>();
 		ArrayList<String> platsTemp = new ArrayList<String>();
 		ArrayList<String> nomCommande = new ArrayList<String>();
-		ArrayList<String> platsCommande = new ArrayList<String>(); 
+		ArrayList<String> platsCommande = new ArrayList<String>();
+		String categorie = new String();
 		double[] prixTemp = new double[15];
 		double[] combien = new double[15];
-		int i = 1;
-		while ( facture[i].compareToIgnoreCase(PLAT) != 0){
-			clientsTemp.add(facture[i]);
-			i++;	
-		}
-		i++;
-		int j = 0;
-		while (facture[i].compareToIgnoreCase(COMMANDE) != 0 ){
-			platsTemp.add(facture[i].split(" ")[0]);
-			try{
-				prixTemp[j] = Double.parseDouble((facture[i].split(" ")[1]));
-			}
-			catch (NumberFormatException e){ 
-				retour = 0;
-			}
-			
-			j++;
-			i++;
-		}
-		i++;
 		int k = 0;
-		while (facture[i].compareToIgnoreCase(FIN) != 0){
-			nomCommande.add(facture[i].split(" ")[0]);
-			platsCommande.add(facture[i].split(" ")[1]);
-			try {
-				combien[k] = Integer.parseInt(facture[i].split(" ")[2]);
-			}
-			catch (Exception e){
-				retour = 1;
-			}
-			i++;
-			k++;
-		}
-		boolean trouve = false;
-		for (String nom : nomCommande) {  
-			trouve = false;
-			for (String vraiNom : clientsTemp) {
-				if (vraiNom.compareToIgnoreCase(nom) == 0){ 
-					trouve = true;
+		int j = 0;
+		for ( int i = 0; i < facture.length; i++ ) {
+
+			switch ( facture[i] ) {
+			case PLAT:
+				categorie = PLAT;
+				break;
+			case COMMANDE:
+				categorie = COMMANDE;
+				break;
+			case CLIENT:
+				categorie = CLIENT;
+				break;
+			case FIN:
+				categorie = FIN;
+				break;
+
+			default:
+				if ( categorie.compareToIgnoreCase( CLIENT ) == 0 ) {
+					clientsTemp.add( facture[i] );
 				}
-			}
-			if (!trouve){
-				retour = 2;
+
+				if ( categorie.compareToIgnoreCase( PLAT ) == 0 ) {
+					platsTemp.add( facture[i].split( " " )[0] );
+					try {
+						prixTemp[j] = Double.parseDouble( ( facture[i].split( " " )[1] ) );
+					} catch ( NumberFormatException e ) {
+						erreursFacture += "Erreur le plat : " + platsTemp.get( j )
+								+ " ne possède pas un prix valide\n\n";
+						facture[i] = "";
+						retour = 0;
+					}
+
+					j++;
+				}
+
+				if ( categorie.compareToIgnoreCase( COMMANDE ) == 0 ) {
+					nomCommande.add( facture[i].split( " " )[0] );
+					platsCommande.add( facture[i].split( " " )[1] );
+					try {
+						combien[k] = Integer.parseInt( facture[i].split( " " )[2] );
+					} catch ( Exception e ) {
+						erreursFacture += "Erreur la commande : " + facture[i]
+								+ " n'est pas écrit sous un format valide\n\n";
+						facture[i] = "";
+						retour = 1;
+					}
+					k++;
+					boolean trouve = false;
+					for ( String nom : nomCommande ) {
+						trouve = false;
+						for ( String vraiNom : clientsTemp ) {
+							if ( vraiNom.compareToIgnoreCase( nom ) == 0 ) {
+								trouve = true;
+							}
+						}
+						if ( !trouve ) {
+							erreursFacture += "Erreur le client : " + nom + " n'est pas un client valide\n\n";
+							facture[i] = "";
+							retour = 2;
+							break;
+						}
+					}
+					boolean trouvePlat = false;
+					for ( String plat : platsCommande ) {
+						trouvePlat = false;
+						for ( String vraiPlat : platsTemp ) {
+							if ( plat.compareToIgnoreCase( vraiPlat ) == 0 ) {
+								trouvePlat = true;
+							}
+						}
+						if ( !trouvePlat ) {
+							erreursFacture += "Erreur le plat : " + plat + " n'est pas un plat valide\n\n";
+							facture[i] = "";
+							retour = 3;
+							;
+						}
+					}
+				}
 				break;
 			}
+
 		}
-		boolean trouvePlat = false;
-		for (String plat : platsCommande) {
-			trouvePlat = false;
-			for (String  vraiPlat : platsTemp) {
-				if (plat.compareToIgnoreCase(vraiPlat) == 0){
-					trouvePlat = true;
-				}
-			}
-			if (!trouvePlat){
-				retour = 3;;
-			}
-		}
-		
+		setErreurs( erreursFacture );
 		return retour;
 	}
-
-
-
 }
--- src/test/TestTesterFormat.java
+++ src/test/TestTesterFormat.java
@@ -11,12 +11,13 @@ public class TestTesterFormat {
 	String[] factureTest = { "Clients :", "Roger", "Céline", "Steeve", "Plats :", "Poutine 10.5", "Frites 2.5",
 			"Repas_Poulet 15.75", "Commandes :", "Roger Poutine 1", "Céline Frites 2", "Céline Repas_Poulet 1",
 			"Fin", };
+	String facture = new String();
 	int result;
-
+	
 	@Test
 	public void testBonneFacture() {
 		result = -1;  
-		assertEquals( result, Facture.testerFormat( factureTest ) );    
+		assertEquals( result, Facture.testerFormat( factureTest, facture ) );    
 		
 	}
 
@@ -24,27 +25,27 @@ public class TestTesterFormat {
 	public void testMauvaisPrix() {
 		factureTest[5] = "Poutine &/!"; 
 		result = 0;
-		assertEquals( result, Facture.testerFormat( factureTest ) );
+		assertEquals( result, Facture.testerFormat( factureTest, facture  ) );
 	}
 
 	@Test
 	public void testMauvaisFormat() {
 		factureTest[11] = "Céline Repas_Poulet 1%"; 
 		result = 1;
-		assertEquals( result, Facture.testerFormat( factureTest ) );
+		assertEquals( result, Facture.testerFormat( factureTest, facture  ) );
 	}
 
 	@Test
 	public void testMauvaisClient() {
 		factureTest[11] = "Simon Repas_Poulet 1";
 		result = 2;
-		assertEquals( result, Facture.testerFormat( factureTest ) );
+		assertEquals( result, Facture.testerFormat( factureTest, facture ) );
 	}
 
 	@Test
 	public void testMauvaisPlat() {
 		factureTest[11] = "Céline Soupe_Poulet_Et_Nouilles 1";
 		result = 3;
-		assertEquals( result, Facture.testerFormat( factureTest ) );
+		assertEquals( result, Facture.testerFormat( factureTest, facture  ) );
 	}
 }
